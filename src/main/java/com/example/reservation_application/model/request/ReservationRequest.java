542ede2c94c57d207d98c6604d47f04e41a5c6fb
omurbayramov <bayramovomur@gmail.com>
2025-08-08 12:50:06 +0400

    Added min, max name length

--- src/main/java/com/example/reservation_application/model/request/ReservationRequest.java
+++ src/main/java/com/example/reservation_application/model/request/ReservationRequest.java
@@ -3,6 +3,7 @@ package com.example.reservation_application.model.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,10 +13,11 @@ public class ReservationRequest {
     private Integer membersCount;
 
     @NotBlank(message = "validation.customerName.blank")
+    @Size(min = 3, max = 30, message = "validation.customerName.size")
     private String customerName;
 
     @NotBlank(message = "validation.reservationDate.blank")
-    private String reservationDate;
+    private String reservationDate; //change to local date., sue value of. delete database entries.
 
     @NotBlank(message = "validation.reservationTime.blank")
     private String reservationTime;
